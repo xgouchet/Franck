5d64f68036d0d450b0bbdf15c72359298e3b19fa
Xavier GOUCHET <android@xgouchet.fr>
2013-02-28 10:24:17 +0100

    Menu select chords

---
+++ Franck/src/fr/xgouchet/musichelper/model/ChordType.java
@@ -0,0 +1,10 @@
+package fr.xgouchet.musichelper.model;
+
+public enum ChordType {
+	major, minor, diminished, augmented
+	
+	/** 
+	 *  TODO 7, m7, 7dim, 7aug,
+	 *  TODO halfdim7, minmaj7, maj7, augmaj7
+	 */
+}
