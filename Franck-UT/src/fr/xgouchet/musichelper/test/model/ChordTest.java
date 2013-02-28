01a598bda7b1cf4a1af034cd38f8797a8d965aed
Xavier GOUCHET <android@xgouchet.fr>
2013-02-28 18:02:33 +0100

    Chord Test

--- Franck-UT/src/fr/xgouchet/musichelper/test/model/ChordTest.java
+++ Franck-UT/src/fr/xgouchet/musichelper/test/model/ChordTest.java
@@ -1,11 +1,12 @@
 package fr.xgouchet.musichelper.test.model;
 
-import java.util.Arrays;
-
 import junit.framework.TestCase;
 import fr.xgouchet.musichelper.model.Chord;
 import fr.xgouchet.musichelper.model.Tone;
 
+/**
+ * TODO test Parse, tostring->parse
+ */
 public class ChordTest extends TestCase {
 
 	/**
@@ -13,68 +14,73 @@ public class ChordTest extends TestCase {
 	 * as the {@link Chord#buildChord(Tone)} from the corresponding chord type
 	 */
 	public void testStaticChords() {
-		Tone staticChord[], enumChord[];
+		Chord staticChord, enumChord;
 
 		// Major
 		staticChord = Chord.buildMajorChord(Tone.C);
 		enumChord = Chord.buildChord(Chord.Type.major, Tone.C);
-		assertTrue("Static vs Enum build chords : major",
-				Arrays.equals(staticChord, enumChord));
+		assertEquals("Static vs Enum build chords : major", staticChord,
+				enumChord);
 
 		// Minor
 		staticChord = Chord.buildMinorChord(Tone.C);
 		enumChord = Chord.buildChord(Chord.Type.minor, Tone.C);
-		assertTrue("Static vs Enum build chords : minor",
-				Arrays.equals(staticChord, enumChord));
+		assertEquals("Static vs Enum build chords : minor", staticChord,
+				enumChord);
 
 		// Aug
 		staticChord = Chord.buildAugmentedChord(Tone.C);
 		enumChord = Chord.buildChord(Chord.Type.augmented, Tone.C);
-		assertTrue("Static vs Enum build chords : aug",
-				Arrays.equals(staticChord, enumChord));
+		assertEquals("Static vs Enum build chords : aug", staticChord,
+				enumChord);
 
 		// Dim
 		staticChord = Chord.buildDiminishedChord(Tone.C);
 		enumChord = Chord.buildChord(Chord.Type.diminished, Tone.C);
-		assertTrue("Static vs Enum build chords : dim",
-				Arrays.equals(staticChord, enumChord));
+		assertEquals("Static vs Enum build chords : dim", staticChord,
+				enumChord);
 
 		// 7th
 		staticChord = Chord.buildSeventhChord(Tone.C);
 		enumChord = Chord.buildChord(Chord.Type.seventh, Tone.C);
-		assertTrue("Static vs Enum build chords : 7th",
-				Arrays.equals(staticChord, enumChord));
+		assertEquals("Static vs Enum build chords : 7th", staticChord,
+				enumChord);
 	}
 
 	/**
 	 * Tests the generation of known chords
 	 */
 	public void testChordValues() {
-		Tone value[], expected[];
+		Chord value, expected;
 
 		// C major
 		value = Chord.buildChord(Chord.Type.major, Tone.C);
-		expected = new Tone[] { Tone.C, Tone.E, Tone.G };
-		assertTrue("Chord C", Arrays.equals(value, expected));
+		expected = new Chord(Tone.C, Chord.Type.major, new Tone[] { Tone.C,
+				Tone.E, Tone.G });
+		assertEquals("Chord C", value, expected);
 
 		// A minor
 		value = Chord.buildChord(Chord.Type.minor, Tone.A);
-		expected = new Tone[] { Tone.A, Tone.C, Tone.E };
-		assertTrue("Chord Am", Arrays.equals(value, expected));
+		expected = new Chord(Tone.A, Chord.Type.minor, new Tone[] { Tone.A,
+				Tone.C, Tone.E });
+		assertEquals("Chord Am", value, expected);
 
-		// F Aug
-		value = Chord.buildChord(Chord.Type.augmented, Tone.F);
-		expected = new Tone[] { Tone.F, Tone.A, Tone.CSharp };
-		assertTrue("Chord Faug", Arrays.equals(value, expected));
+		// C Aug
+		value = Chord.buildChord(Chord.Type.augmented, Tone.C);
+		expected = new Chord(Tone.C, Chord.Type.augmented, new Tone[] { Tone.C,
+				Tone.E, Tone.GSharp });
+		assertEquals("Chord Faug", value, expected);
 
 		// D Dim
 		value = Chord.buildChord(Chord.Type.diminished, Tone.D);
-		expected = new Tone[] { Tone.D, Tone.F, Tone.GSharp };
-		assertTrue("Chord Ddim", Arrays.equals(value, expected));
+		expected = new Chord(Tone.D, Chord.Type.diminished, new Tone[] {
+				Tone.D, Tone.F, Tone.GSharp });
+		assertEquals("Chord Ddim", value, expected);
 
 		// E dominant 7th
 		value = Chord.buildChord(Chord.Type.seventh, Tone.E);
-		expected = new Tone[] { Tone.E, Tone.GSharp, Tone.B, Tone.D };
-		assertTrue("Chord E7", Arrays.equals(value, expected));
+		expected = new Chord(Tone.E, Chord.Type.seventh, new Tone[] { Tone.E,
+				Tone.GSharp, Tone.B, Tone.D });
+		assertEquals("Chord E7", value, expected);
 	}
 }
