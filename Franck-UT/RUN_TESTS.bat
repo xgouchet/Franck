8829abeab7254b0a85592f5c7f007c8ff07b8410
Xavier Gouchet <github@xgouchet.fr>
2013-03-04 22:15:29 +0100

    Popup on test failures

--- Franck-UT/RUN_TESTS.bat
+++ Franck-UT/RUN_TESTS.bat
@@ -1,2 +1,21 @@
-﻿adb uninstall fr.xgouchet.musichelper.test
-ant clean emma debug install test emma-reports > ant.log
+adb uninstall fr.xgouchet.musichelper
+adb uninstall fr.xgouchet.musichelper.test 
+call ant emma clean debug install 
+call ant emma test emma-reports > test.log
+findstr "Failure expected" test.log > test-failures.log
+
+@echo off
+setlocal
+set file="test-failures.log"
+
+FOR /F "usebackq" %%A IN ('%file%') DO set size=%%~zA
+
+IF %size% LEQ 0 goto okay 
+	echo msgbox "Some Unit Tests failed",vbOKOnly,"Franck UT" > %tmp%\tmp.vbs
+	wscript %tmp%\tmp.vbs
+	del %tmp%\tmp.vbs
+:okay
+
+rm -f "test.log"
+@echo on
+
