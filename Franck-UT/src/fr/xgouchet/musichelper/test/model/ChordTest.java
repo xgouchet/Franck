91af70ffd39257966c13b9e72b384c4f3d211e84
Xavier GOUCHET <android@xgouchet.fr>
2013-03-04 17:57:20 +0100

    Chord tests

--- Franck-UT/src/fr/xgouchet/musichelper/test/model/ChordTest.java
+++ Franck-UT/src/fr/xgouchet/musichelper/test/model/ChordTest.java
@@ -133,6 +133,96 @@ public class ChordTest extends TestCase {
 		expected = new Chord(middleC, Chord.Type.diminished7, new Note[] {
 				new Note(0), new Note(3), new Note(6), new Note(9) });
 		assertEquals("Chord Cdim7", value, expected);
+	}
 
+	/**
+	 * Tests the generation of known chords simplified
+	 */
+	public void testChordSimplifiedValues() {
+		Chord value, expected;
+
+		Note middleC = new Note();
+
+		// C major
+		value = Chord.buildChord(Chord.Type.major, middleC);
+		expected = new Chord(middleC, Chord.Type.major, new Note[] {
+				new Note(0), new Note(4), new Note(7) });
+		expected.simplify();
+		assertEquals("Chord C", value, expected);
+
+		// C minor
+		value = Chord.buildChord(Chord.Type.minor, middleC);
+		expected = new Chord(middleC, Chord.Type.minor, new Note[] {
+				new Note(0), new Note(3), new Note(7) });
+		expected.simplify();
+		assertEquals("Chord Cm", value, expected);
+
+		// C Aug
+		value = Chord.buildChord(Chord.Type.augmented, middleC);
+		expected = new Chord(middleC, Chord.Type.augmented, new Note[] {
+				new Note(0), new Note(4), new Note(8) });
+		expected.simplify();
+		assertEquals("Chord Caug", value, expected);
+
+		// C Dim
+		value = Chord.buildChord(Chord.Type.diminished, middleC);
+		expected = new Chord(middleC, Chord.Type.diminished, new Note[] {
+				new Note(0), new Note(3), new Note(6) });
+		expected.simplify();
+		assertEquals("Chord Cdim", value, expected);
+
+		// C dominant 7th
+		value = Chord.buildChord(Chord.Type.dominant7, middleC);
+		expected = new Chord(middleC, Chord.Type.dominant7, new Note[] {
+				new Note(0), new Note(4), new Note(7), new Note(10) });
+		expected.simplify();
+		assertEquals("Chord C7", value, expected);
+
+		// C major 7th
+		value = Chord.buildChord(Chord.Type.major7, middleC);
+		expected = new Chord(middleC, Chord.Type.major7, new Note[] {
+				new Note(0), new Note(4), new Note(7), new Note(11) });
+		expected.simplify();
+		assertEquals("Chord CM7", value, expected);
+
+		// C minor 7th
+		value = Chord.buildChord(Chord.Type.minor7, middleC);
+		expected = new Chord(middleC, Chord.Type.minor7, new Note[] {
+				new Note(0), new Note(3), new Note(7), new Note(10) });
+		expected.simplify();
+		assertEquals("Chord Cm7", value, expected);
+
+		// C augmented 7th
+		value = Chord.buildChord(Chord.Type.augmented7, middleC);
+		expected = new Chord(middleC, Chord.Type.augmented7, new Note[] {
+				new Note(0), new Note(4), new Note(8), new Note(10) });
+		expected.simplify();
+		assertEquals("Chord Caug7", value, expected);
+
+		// C diminished 7th
+		value = Chord.buildChord(Chord.Type.diminished7, middleC);
+		expected = new Chord(middleC, Chord.Type.diminished7, new Note[] {
+				new Note(0), new Note(3), new Note(6), new Note(9) });
+		expected.simplify();
+		assertEquals("Chord Cdim7", value, expected);
+
+	}
+
+	/**
+	 * Test misc getters to chord fields
+	 */
+	public void testGetters() {
+		Note middleC = new Note();
+
+		// C augmented 7th
+		Chord chord = Chord.buildChord(Chord.Type.augmented7, middleC);
+
+		assertEquals("getDominant", middleC, chord.getDominant());
+		assertEquals("getType", Chord.Type.augmented7, chord.getType());
+
+		Note[] notes = chord.getNotes();
+		assertEquals("getNotes lenght", 4, notes.length);
+
+		assertTrue("hasAlteration", chord.hasAlteration());
 	}
 }
