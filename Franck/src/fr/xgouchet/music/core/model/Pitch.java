7ab54c7077283eadc1d4173aa881d51ac2fa8548
Xavier Gouchet <xg@deezer.com>
2014-02-07 15:04:04 +0100

    Initial Code

---
+++ Franck/src/fr/xgouchet/music/core/model/Pitch.java
@@ -0,0 +1,55 @@
+package fr.xgouchet.music.core.model;
+
+/**
+ * Describes the 7 different base pitches known in music
+ * 
+ * @author Xavier Gouchet
+ */
+public enum Pitch {
+
+	/** */
+	C(0, "Do"),
+	/** */
+	D(2, "Re"),
+	/** */
+	E(4, "Mi"),
+	/** */
+	F(5, "Fa"),
+	/** */
+	G(7, "Sol"),
+	/** */
+	A(9, "La"),
+	/** */
+	B(11, "Si");
+
+	/** The number of half tones between this pitch and the natural C */
+	private final int mHalfTones;
+
+	/** The neolatin name of the pitch */
+	private final String mNeoLatinName;
+
+	private Pitch(int halfTones, String neoLatinName) {
+		mHalfTones = halfTones;
+		mNeoLatinName = neoLatinName;
+	}
+
+	/**
+	 * @return the halftones count from a natural C to this pitch (assuming they
+	 *         are in the same octave)
+	 */
+	public int getHalfTones() {
+		return mHalfTones;
+	}
+
+	/**
+	 * @return the name of the current pitch in the given notation
+	 */
+	public String getPitchName(boolean neolatinName) {
+		if (neolatinName) {
+			return mNeoLatinName;
+		} else {
+			return name();
+		}
+	}
+
+}
---
+++ Franck/src/fr/xgouchet/music/core/model/Symbols.java
@@ -0,0 +1,17 @@
+package fr.xgouchet.music.core.model;
+
+/**
+ * 
+ * @author Xavier Gouchet
+ * 
+ */
+public class Symbols {
+
+	public static final char FLAT_SIGN = 0x266D;
+	public static final char NATURAL_SIGN = 0x266E;
+	public static final char SHARP_SIGN = 0x266F;
+
+	public static final char QUARTER_NOTE = 0x2669;
+	public static final char EIGTH_NOTE = 0x266A;
+
+}
